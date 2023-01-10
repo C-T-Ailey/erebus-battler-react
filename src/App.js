42348bebbfa0b1be595a0f05c40556d928415553
Tom Biscuit <chris.ailey90@gmail.com>
2023-01-10 22:58:53 +0000

    created Troop class, working on adding troops to an object dictionary instead of simple counter, attempting modifiers for variants among same category troops

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ function App() {
 
   const [points, setPoints] = useState(1000)
 
+  const [factionIsChosen, setFactionIsChosen] = useState(false)
+
+  const [faction, setFaction] = useState("Not selected")
+
+  useEffect(() => {
+    console.log(factionIsChosen)
+    console.log(faction)
+  }, [factionIsChosen, faction])
+
   useEffect(() => {
     console.log(points)
   }, [points])
+
+  const factionSelect = (e) => {
+    console.log(e.target.name)
+    setFactionIsChosen(true)
+    setFaction(e.target.name)
+  }
 
   return (
     // <div className="App">
@@ -30,13 +45,20 @@ function App() {
     // </div>
     <div>
       <div>
-        Points: {points}
+        <p>Faction: {faction}</p>
+        <p>Points: {points}</p>
       </div>
       <div>
-        <Units points={points} setPoints={setPoints}></Units>
-      </div>
-      <div>
-        <p>Display</p>
+        { !factionIsChosen 
+        ? 
+        <div>
+          <p>Choose a faction:</p> 
+          <button name='The Tyranny' onClick={(e) => factionSelect(e)}>Tyranny</button>
+          <button name='The Necropolis' onClick={(e) => factionSelect(e)}>Necropolis</button>
+        </div>
+        :
+        <Units points={points} setPoints={setPoints} faction={faction}></Units>
+        }
       </div>
     </div>
   );
